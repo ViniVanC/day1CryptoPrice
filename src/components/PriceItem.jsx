ae1e307d4c94bb7faa05556e8c81d8031ad966ae
ViniVanC <vincentvan0302@gmail.com>
2023-02-21 19:43:44 +0200

    created components

---
+++ src/components/PriceItem.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PriceItem = () => {
+  return <div>PriceItem</div>;
+};
---
+++ src/components/PriceList.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { PriceItem } from "./PriceItem";
+
+export const PriceList = () => {
+  return <PriceItem />;
+};
